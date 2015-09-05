4b5a2aa167d42fce311f2f5694b9b2ed78549332
serandomps <serandomps@gmail.com>
2015-09-05 21:06:35 +0530

    updating

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var serand = require('serand');
 var navigation = require('navigation');
 
+var links = {
+    home: {
+        url: '/',
+        title: 'autos'
+    },
+    menu: [{
+        url: 'https://autos.serandives.com',
+        title: 'Autos'
+    }, {
+        url: 'https://hotels.serandives.com',
+        title: 'Hotels'
+    }, {
+        url: 'https://jobs.serandives.com',
+        title: 'Jobs'
+    }, {
+        url: 'https://states.serandives.com',
+        title: 'Real States'
+    }]
+};
+
 var render = function () {
     $.ajax({
         url: '/apis/v/menus/0',
         headers: {
-            'x-host': 'accounts.serandives.com'
+            'x-host': 'autos.serandives.com'
         },
         dataType: 'json',
         success: function (data) {
@@ -18,15 +38,12 @@ var render = function () {
 };
 
 module.exports = function (sandbox, fn, options) {
-    navigation(sandbox, fn, options);
-    render();
+    navigation(sandbox, fn, links);
 };
-
 
 serand.on('user', 'logged in', function (usr) {
     render();
 });
-
 
 serand.on('user', 'logged out', function (usr) {
     render();
