450a9d197f49614b8c2fc89599b11bc814d15928
serandomps <serandomps@gmail.com>
2015-10-19 13:18:18 +0530

    updating

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
 var serand = require('serand');
 var navigation = require('navigation');
 
+var context;
+
+var ready = false;
+
 var render = function (done) {
     $.ajax({
         url: '/apis/v/menus/0',
         headers: {
-            'x-host': 'autos.serandives.com'
+            'x-host': 'accounts.serandives.com'
         },
         dataType: 'json',
-        success: function (data) {
-            done(false, data);
+        success: function (links) {
+            done(false, links);
         },
         error: function () {
-            done(true);
+            done(true, {});
         }
     });
 };
 
 module.exports = function (sandbox, fn, options) {
-    render(function (err, links) {
+    context = {
+        sandbox: sandbox,
+        fn: fn
+    };
+    if (!ready) {
+        return;
+    }
+    render(function(err, links) {
         navigation(sandbox, fn, links);
     });
 };
 
-serand.on('user', 'logged in', function () {
-    render(function (err, links) {
-        serand.emit('navigation', 'render', links);
+serand.on('user', 'ready', function (user) {
+    ready = true;
+    if (!context) {
+        return;
+    }
+    render(function(err, links) {
+        navigation(context.sandbox, context.fn, links);
     });
 });
 
-serand.on('user', 'logged out', function () {
-    render(function (err, links) {
-        serand.emit('navigation', 'render', links);
-    });
-});
+/*serand.on('user', 'logged in', function () {
+ console.log('----------------------------2');
+ render(function(err, links) {
+ serand.emit('navigation', 'render', links);
+ });
+ });
+
+ serand.on('user', 'logged out', function () {
+ console.log('----------------------------3');
+ render(function(err, links) {
+ serand.emit('navigation', 'render', links);
+ });
+ });*/
